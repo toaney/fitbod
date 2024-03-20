00f5ded1f02f040c25268685e6dc2c271243aa9a
toaney <toaney@gmail.com>
2024-03-20 12:16:53 -0700

    fix build errors

--- src/app/exercise/page.tsx
+++ src/app/exercise/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useState, useEffect, useRef} from "react";
+import React, {useState, useEffect, useRef, Suspense} from "react";
 import { useSearchParams } from "next/navigation";
 import Image from "next/image";
 
@@ -13,88 +13,34 @@ interface performance {
   estimated1RM: number,
 }
 
+interface performanceGroup {
+  id: string,
+  data: performance[]
+}
+
+interface formErrors {
+  repsInput: boolean,
+  weightInput: boolean
+}
+
 const ExercisePage = () => {
   const searchParams = useSearchParams();
 
-  const exerciseId : any = searchParams.get("id")? searchParams.get("id") : "";
-  const exerciseName : any = searchParams.get("name")? searchParams.get("name") : "";
-  const exerciseImage : any = searchParams.get("thumb")? searchParams.get("thumb") : "";
+  const exerciseId : string | null = searchParams.get("id")? searchParams.get("id") : "";
+  const exerciseName : string | null = searchParams.get("name")? searchParams.get("name") : "";
+  const exerciseImage : string | null = searchParams.get("thumb")? searchParams.get("thumb") : "";
   const [ hasMounted, setHasMounted ] = useState<boolean>(false);
-  const [ reps, setReps ] = useState("");
-  const [ weight, setWeight ] = useState("");
-  const [ performanceList, setPerformanceList ] = useState([]);
-  const [ errors, setErrors ] = useState("");
-  const inputRef = useRef(null);
-  const errorRef = useRef(null);
-  // const [ errors, setErrors ] = useState({
-  //   repsInput: false,
-  //   weightInput: false
-  // })  
-
-  //   [{
-  //   id: "03/23/2024",
-  //   data: [
-  //     {
-  //       "exerciseId": "lat-pulldown",
-  //       "time": "2024-03-19T23:47:08.683Z",
-  //       "reps": "10",
-  //       "weight": "20",
-  //       "estimated1RM": 27
-  //     },
-  //     {
-  //       "exerciseId": "lat-pulldown",
-  //       "time": "2024-03-19T23:46:44.474Z",
-  //       "reps": "10",
-  //       "weight": "60",
-  //       "estimated1RM": 80
-  //     },
-  //     {
-  //       "exerciseId": "lat-pulldown",
-  //       "time": "2024-03-19T23:45:53.590Z",
-  //       "reps": "5",
-  //       "weight": "70",
-  //       "estimated1RM": 79
-  //     },
-  //     {
-  //       "exerciseId": "lat-pulldown",
-  //       "time": "2024-03-19T23:04:32.878Z",
-  //       "reps": "12",
-  //       "weight": "50",
-  //       "estimated1RM": 72
-  //     }
-  //   ]
-  // }, {
-  //   id: "03/22/2024",
-  //   data: [
-  //     {
-  //       "exerciseId": "lat-pulldown",
-  //       "time": "2024-03-19T23:47:08.683Z",
-  //       "reps": "10",
-  //       "weight": "20",
-  //       "estimated1RM": 27
-  //     },
-  //     {
-  //       "exerciseId": "lat-pulldown",
-  //       "time": "2024-03-19T23:46:44.474Z",
-  //       "reps": "10",
-  //       "weight": "60",
-  //       "estimated1RM": 80
-  //     },
-  //     {
-  //       "exerciseId": "lat-pulldown",
-  //       "time": "2024-03-19T23:45:53.590Z",
-  //       "reps": "5",
-  //       "weight": "70",
-  //       "estimated1RM": 79
-  //     }
-  //   ]
-  // }]
-  
-
+  const [ reps, setReps ] = useState<number | null>(null);
+  const [ weight, setWeight ] = useState<number | null>(null);
+  const [ performanceList, setPerformanceList ] = useState<performanceGroup[]>([]);
+  const [ errors, setErrors ] = useState<formErrors | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   useEffect (() => {
     setHasMounted(true)
-    inputRef.current.focus()
+    if (inputRef.current !== null){
+      inputRef.current.focus()
+    }
   }, [])
 
   useEffect (() => {
@@ -107,11 +53,14 @@ const ExercisePage = () => {
     if (hasMounted){
       setPerformanceList(localStorage.getItem(`fitbod-${exerciseId}`) ? JSON.parse(localStorage.getItem(`fitbod-${exerciseId}`) || "{}") : [])
     }
-  }, [hasMounted])
+  }, [hasMounted, exerciseId])
 
   function formErrors(){
     let hasErrors = false;
-    let newErrors = {}
+    let newErrors = {
+      repsInput: false,
+      weightInput: false
+    }
 
     if (!reps) {
       newErrors.repsInput = true;
@@ -124,16 +73,14 @@ const ExercisePage = () => {
     }
     
     setErrors(newErrors);
-    
-
     return hasErrors;
   }
 
   function getDate(){
     const today = new Date();
-    const yyyy = today.getFullYear();
-    let mm = today.getMonth() + 1;
-    let dd = today.getDate();
+    const yyyy : string | number = today.getFullYear();
+    let mm : string | number = today.getMonth() + 1;
+    let dd : string | number = today.getDate();
 
     if (dd < 10) dd = "0" + dd;
     if (mm < 10) mm = "0" + mm;
@@ -141,26 +88,25 @@ const ExercisePage = () => {
     return mm + "/" + dd + "/" + yyyy;
   }
 
-  function handleSubmit(e){
+  function handleSubmit(e : any){
     e.preventDefault();
 
     if (!formErrors()){
       let currentDate = getDate();
-      let calculatedRM : number = Math.round(weight * (36/ (37 - reps)))
-      let newPerformance = {
+      let calculatedRM : number = weight && reps ? Math.round(weight * (36/ (37 - reps))) : 0;
+      let newPerformance : performance = {
         id: crypto.randomUUID(),
-        exerciseId: exerciseId,
         time: new Date(),
-        reps: reps,
-        weight: weight,
+        reps: reps || 0,
+        weight: weight || 0,
+        exerciseId: exerciseId || "",
         estimated1RM: calculatedRM,
       }
 
-
-      if (performanceList.some(item => item.id === currentDate)) {
+      if (performanceList.some((item: performanceGroup) => item.id === currentDate)) {
         // date id found
-        setPerformanceList((currentList : performance[]) => {
-          return currentList.map(item => {
+        setPerformanceList((currentList : performanceGroup[]) => {
+          return currentList.map((item: performanceGroup) => {
             if (item.id === currentDate){
               return {
                 ...item, data: [newPerformance, ...item.data]
@@ -171,13 +117,13 @@ const ExercisePage = () => {
           })
         })
 
-        setReps("");
-        setWeight("");
-        setErrors("");
+        setReps(null);
+        setWeight(null);
+        setErrors(null);
 
       } else {
         // date id not found
-        setPerformanceList((currentList : performance[]) => {
+        setPerformanceList((currentList: performanceGroup[]) => {
           let newPerformanceGroup = {
             id : currentDate,
             data: [ newPerformance ]
@@ -185,36 +131,40 @@ const ExercisePage = () => {
           return [newPerformanceGroup, ...currentList]
         })
     
-        setReps("");
-        setWeight("");
-        setErrors("");
+        setReps(null);
+        setWeight(null);
+        setErrors(null);
       }
-      inputRef.current.focus()
+      if (inputRef.current !== null){
+        inputRef.current.focus()
+      }
     } else {
-      inputRef.current.focus()
+      if (inputRef.current !== null){
+        inputRef.current.focus()
+      }
     }
   }
 
   return (
-    // <div className="w-full h-full flex flex-col items-center">
+    <Suspense>
       <div className="flex flex-col items-center">
         <h1 className="flex pt-[32px] pb-[48px] justify-center text-[24px]">{exerciseName}</h1>
         <div className="relative w-[150px] h-[150px] ">
-          <Image src={exerciseImage} priority fill alt={exerciseName} className="rounded-lg bg-slate-50 object-cover"/>
+          <Image src={exerciseImage ? exerciseImage : ""} priority fill alt={exerciseName ? exerciseName : ""} className="rounded-lg bg-slate-50 object-cover"/>
         </div>
         <div className="w-full px-8 md:w-[548px] md:px-0 pb-8">
           <form onSubmit={handleSubmit}>
           <div className="text-base font-bold mt-8 mb-6">Add set:</div>
-            {errors && <div id="reps-error" tabIndex={0} role="alert" autoFocus ref={errorRef} className={`text-red-600 text-sm mb-2`}>Please enter values for both input fields</div>}
+            {errors && <div id="reps-error" tabIndex={0} role="alert" autoFocus className={`text-red-600 text-sm mb-2`}>Please enter values for both input fields</div>}
             <div className="flex flex-row justify-between items-end sm:justify-normal">
               <div className="flex">
                 <div>
                   <label htmlFor="reps" className="mr-2 text-sm">Reps</label>
-                  <input type="number" value={reps} ref={inputRef} id="reps" onChange={(e) => setReps(e.target.value)} className={`${errors.repsInput ? "border-red-600 ring-red-600 hover:focus:border-red-600 " : "border-black"} border rounded-[12px] h-[35px] w-[72px] px-2`}/>
+                  <input type="number" value={reps ? reps : ""} ref={inputRef} id="reps" onChange={(e) => setReps(e.target.valueAsNumber)} className={`${errors ? errors.repsInput ? "border-red-600" : "border-black" : ""} border rounded-[12px] h-[35px] w-[72px] px-2`}/>
                 </div>
                 <div className="ml-4">
                   <label htmlFor="weight" className="mr-2 text-sm">Weight</label>
-                  <input type="number" value={weight} id="weight" onChange={(e) => setWeight(e.target.value)} className={`${errors.weightInput ? "border-red-600" : "border-black"} focus-visible:ring-0 border rounded-[12px] h-[35px] w-[72px] px-2`}/>
+                  <input type="number" value={weight ? weight : ""} id="weight" onChange={(e) => setWeight(e.target.valueAsNumber)} className={`${errors ? errors.weightInput ? "border-red-600" : "border-black": ""} border rounded-[12px] h-[35px] w-[72px] px-2`}/>
                 </div>
               </div>
               <button className="bg-[#0085BF] h-[35px] w-[42px] rounded-[8px] ml-3 sm:ml-8 pb-[2px] font-[24] font-normal text-white">+</button>
@@ -222,7 +172,7 @@ const ExercisePage = () => {
           </form>
           <div>
             <div className="text-base font-bold mt-8 mb-2">Performances:</div>
-            {performanceList.map(date => {
+            {performanceList.map((date: performanceGroup) => {
               return (
                 <div key={date.id} className="mt-6 text-sm">
                   <div className="font-bold">{date.id}</div>
@@ -243,19 +193,13 @@ const ExercisePage = () => {
                       )
                     })}
                   </ul>
-
-
                 </div>
-
               )
             })}
-
-
           </div>
         </div>
-
       </div>
-    // </div>
+    </Suspense>
   )
 }
 
