e61584362c5657d873e03a7cdba7c1cd93f07281
toaney <toaney@gmail.com>
2024-03-22 14:33:49 -0700

    index page code cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,9 @@
-// "use client" 
-
-import fetchExercises from "@/unused_test_code/fetchExercises/fetchExercises"
 import type { Exercise } from "@/types/Exercise"
-import { useState, useEffect } from "react"
 import ExerciseList from "./components/ExerciseList/ExerciseList"
 
 const IndexPage = async () => {
   const res = await fetch("https://storage.googleapis.com/fitbod-web-internal/exercises.json");
   const exercises: Exercise[] = await res.json();
-  // const [exerciseList, setExerciseList] = useState<Exercise[]>([])
-
-  // const exercises: any = fetchExercises()
-
-  // useEffect(() => {
-  //   async function getExercises() {
-  //     // const exercises = await fetchExercises()
-  //     const res = await fetch("https://storage.googleapis.com/fitbod-web-internal/exercises.json", {
-  //       mode: 'no-cors',
-  //       // method: "get",
-  //       headers: {
-  //            "Content-Type": "application/json"
-  //       },
-  //       // body: JSON.stringify(ob)
-  //     });
-  //     const exercises: Exercise[] = await res.json();
-  //     if (exercises?.length) setExerciseList(exercises)
-  //   }
-
-  //   getExercises()
-  // }, [])
 
   return (
     <div className="w-full h-full flex flex-col items-center">
